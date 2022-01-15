7af5ba6feed718bbb6e755523c8daa1b032b1984
JhonyLemon <76851495+JhonyLemon@users.noreply.github.com>
2022-01-15 19:45:10 +0100

    naprawa logowania znowu

--- StacjaBenzynowaMVVM/ViewModels/LoginViewModel.cs
+++ StacjaBenzynowaMVVM/ViewModels/LoginViewModel.cs
@@ -74,7 +74,7 @@ namespace StacjaBenzynowa.ViewModels
         public void LogIn()
         {
             Employee= DatabaseDataHelper.GetEmployee(UserName, Password);
-            if (Employee == null)
+            if (Employee == null || Employee.ID_PRACOWNIKA==0)
             {
                 ErrorMessage = "Błędny login lub hasło";
             }
